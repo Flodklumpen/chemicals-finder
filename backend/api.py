f099e1631dd6a02012390fa717e8495bc1d33c00
Felicia <flisanflod@gmail.com>
2024-07-02 22:01:02 +0200

    Add CSS and display result

--- backend/api.py
+++ backend/api.py
@@ -1,8 +1,15 @@
 import time
-from flask import Flask
+from flask import Flask, request
+from chemicals_finder import find_chemicals
 
 app = Flask(__name__)
+
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+
+@app.route('/chemicals', methods=['GET'])
+def investigate_input():
+    return find_chemicals(request.args.get('input'))
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,32 +2,156 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const [currentTime, setCurrentTime] = useState(0);
+/*
+* test data
+* bha, bht, phmb, ptfe, polytef, acrylate, nylon, cyclotetrasiloxane
+* */
 
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  }, []);
+function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [resultData, setResultData] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+    console.log('Form submitted with input:', inputValue);
+
+    try {
+      const response = await fetch(`chemicals?input=${encodeURIComponent(inputValue)}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      setResultData(data);
+      console.log('Response data:', data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter' && !event.shiftKey) {
+      event.preventDefault();
+      handleSubmit(event).then();
+    }
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The current time is {currentTime}.</p>
-      </header>
+      <div className="App-header">
+      </div>
+      <div className="body-wrapper">
+        <div className="App-body">
+          <form onSubmit={handleSubmit}>
+          <textarea className="input-textarea"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    onKeyDown={handleKeyDown}
+          />
+          </form>
+          {resultData && (
+            <div className="result">
+              <div className="header-small">
+                <p>Din produkt innehåller:</p>
+              </div>
+
+              <div className="exact-wrapper">
+                <div className="exact-container">
+                  <h4>Exakta matchningar: {resultData.exact_match.number}</h4>
+                  <div className="exact-entry-container">
+                    {resultData.exact_match.pfas.length !== 0 && (
+                      <div>
+                        PFAS änmen:
+                        {Object.keys(resultData.exact_match.pfas).map((key) => (
+                          <div key={key} className="exact-entries">
+                            {resultData.exact_match.pfas[key]}
+                          </div>
+                        ))}
+                      </div>
+                    )}
+                    {resultData.exact_match.hormone.length !== 0 && (
+                      <div>
+                          Hormonstörande ämnen:
+                        {Object.keys(resultData.exact_match.hormone).map((key) => (
+                          <div key={key} className="exact-entries">
+                            {resultData.exact_match.hormone[key]}
+                          </div>
+                        ))}
+                      </div>
+                    )}
+                    {resultData.exact_match.plastic.length !== 0 && (
+                      <div>
+                        Plaster:
+                        {Object.keys(resultData.exact_match.plastic).map((key) => (
+                          <div key={key} className="exact-entries">
+                            {resultData.exact_match.plastic[key]}
+                          </div>
+                        ))}
+                      </div>
+                    )}
+                    {resultData.exact_match.cmr.length !== 0 && (
+                      <div>
+                          Ämnen som kan orsaka cancer, genmutationer eller störa
+                          förmågan att få barn (CMR-ämnen):
+                          {Object.keys(resultData.exact_match.cmr).map((key) => (
+                            <div key={key} className="exact-entries">
+                              {resultData.exact_match.cmr[key]}
+                            </div>
+                          ))}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+
+              <div className="partly-container">
+                <p>Delvis matchningar: {resultData.partly_match.number}</p>
+                {resultData.partly_match.pfas.original && (
+                  <div className="partly">
+                    <p>Du skrev in: {resultData.partly_match.pfas.original}</p>
+                    <p>PFAS änmen som delvis matchade: {resultData.partly_match.pfas.matching}</p>
+                  </div>
+                )}
+                {resultData.partly_match.hormone.original && (
+                  <div className="partly">
+                    <p>Du skrev in: {resultData.partly_match.hormone.original}</p>
+                    <p>Hormonstörande ämnen som delvis matchade: {resultData.partly_match.hormone.matching}</p>
+                  </div>
+                )}
+                {resultData.partly_match.plastic.original && (
+                  <div className="partly">
+                    <p>Du skrev in: {resultData.partly_match.plastic.original}</p>
+                    <p>Plastämnen som delvis matchade: {resultData.partly_match.plastic.matching}</p>
+                  </div>
+                )}
+                {resultData.partly_match.cmr.original && (
+                  <div className="partly">
+                    <p>Du skrev in: {resultData.partly_match.cmr.original}</p>
+                    <p>Ämnen som kan orsaka cancer, genmutationer eller störa
+                      förmågan att få barn (CMR-ämnen) som delvis matchade: {resultData.partly_match.cmr.matching}</p>
+                  </div>
+                )}
+              </div>
+
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div className="App-footer">
+      </div>
+
+      {/*<img src={logo} className="App-logo" alt="logo" />*/}
     </div>
   );
 }
